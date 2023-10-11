42bb2487dd3b6b1e31bfc92ce08302f45e91e254
Salam <SalamPopolama@yandex.ru>
2023-10-11 19:14:51 +0300

    Fixed availability of changing constants

--- src/main/java/com/shatun/townystorage/utils/Paths.java
+++ src/main/java/com/shatun/townystorage/utils/Paths.java
@@ -1,20 +1,20 @@
 package com.shatun.townystorage.utils;
 
 public class Paths {
-    public static String DATA = "data/";
-    public static String CONFIGS = "";
+    public static final String DATA = "data/";
+    public static final String CONFIGS = "";
 
 
 
-    public static String YML_SETTINGS_ACCESS_MODE = "access_mode";
-    public static String YML_SETTINGS_DEFAULT_SIZE = "default_storage_size";
-    public static String YML_SETTINGS_AUTOSAVE_PERIOD = "storage_autosave_period";
+    public static final String YML_SETTINGS_ACCESS_MODE = "access_mode";
+    public static final String YML_SETTINGS_DEFAULT_SIZE = "default_storage_size";
+    public static final String YML_SETTINGS_AUTOSAVE_PERIOD = "storage_autosave_period";
 
 
 
-    public static String YML_STORAGE_INVENTORY = "storage.inventory";
-    public static String YML_STORAGE_SIZE = "storage.size";
+    public static final String YML_STORAGE_INVENTORY = "storage.inventory";
+    public static final String YML_STORAGE_SIZE = "storage.size";
 
 
-    public static String TRANSLATION_DEFAULT_STORAGE_NAME = "DEFAULT_STORAGE_NAME";
+    public static final String TRANSLATION_DEFAULT_STORAGE_NAME = "DEFAULT_STORAGE_NAME";
 }
--- src/main/java/com/shatun/townystorage/utils/Perms.java
+++ src/main/java/com/shatun/townystorage/utils/Perms.java
@@ -1,6 +1,6 @@
 package com.shatun.townystorage.utils;
 
 public class Perms {
-    public final static String COMMAND_STORAGE = "townystorage.command.storage";
-    public final static String ADMIN = "townystorage.admin";
+    public static final String COMMAND_STORAGE = "townystorage.command.storage";
+    public static final String ADMIN = "townystorage.admin";
 }
